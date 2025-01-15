38f3b2eef84778047fc172b1951dcfa72a150e42
luisatwistedchaos101 <luisaseafox@gmail.com>
2025-01-15 15:44:03 -0300

    Hue

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -1,11 +1,22 @@
 import Header from '../Header'
 import './style.css'
+import runAway from "../../assets/Run.jpg"
+import seekBar from "../../assets/SeekBar.png"
 
 export default function App() {
   return (
     <>
       <Header title="Tocando Agora" />
-      <h1>Olá mundo</h1>
+      <div>
+      <img src ={runAway} alt="capaMusica"></img>
+        <h2 className = "text-lg w-6 h-6 font-bold">Runaway(U&I)</h2>
+      </div>
+      <div>
+        <h3 className="text-lg w-5 h-5 font-extralight">Galantis</h3>
+      </div>
+      <div>
+        <img className = "" src = {seekBar} alt = "barra"></img>
+      </div>
     </>
   )
 }
